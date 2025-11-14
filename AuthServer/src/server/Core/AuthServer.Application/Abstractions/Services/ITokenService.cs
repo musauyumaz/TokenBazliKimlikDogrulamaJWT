dc64ef7ea25a46742518c2c4f0e8f15bb1a97997
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-11-14 21:48:18 +0300

    feat: add ITokenService interface

---
+++ AuthServer/src/server/Core/AuthServer.Application/Abstractions/Services/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using AuthServer.Application.Features.Auths.DTOs;
+
+namespace AuthServer.Application.Abstractions.Services
+{
+    public interface ITokenService
+    {
+        TokenDTO CreateTokenAsync(string userId, string password);
+        ClientTokenDTO CreateTokenByClientAsync(string userId, string password);
+    }
+}
