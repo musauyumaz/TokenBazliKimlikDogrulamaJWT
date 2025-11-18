2e229f982d4124e3fe2d4160a7b0333abad8b9b2
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-11-18 22:15:29 +0300

    feat: add AuthServerDbContext and dependencies

---
+++ AuthServer/src/server/Infrastructure/AuthServer.Persistence/DbContexts/AuthServerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AuthServer.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Persistence.DbContexts
+{
+    public class AuthServerDbContext(DbContextOptions<AuthServerDbContext> _options) : IdentityDbContext<User,IdentityRole, string>(_options)
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+    }
+}
