500d9fa8e5368eca8bdb0caacb9fe9d71af52892
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-11-18 22:24:54 +0300

    feat: add entity mappings for Product, User, and UserRefreshToken

--- AuthServer/src/server/Infrastructure/AuthServer.Persistence/DbContexts/AuthServerDbContext.cs
+++ AuthServer/src/server/Infrastructure/AuthServer.Persistence/DbContexts/AuthServerDbContext.cs
@@ -5,9 +5,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AuthServer.Persistence.DbContexts
 {
-    public class AuthServerDbContext(DbContextOptions<AuthServerDbContext> _options) : IdentityDbContext<User,IdentityRole, string>(_options)
+    public sealed class AuthServerDbContext(DbContextOptions<AuthServerDbContext> _options) : IdentityDbContext<User,IdentityRole, string>(_options)
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ AuthServer/src/server/Infrastructure/AuthServer.Persistence/EntityMappings/UserRefreshTokenMapping.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthServer.Persistence.EntityMappings
+{
+    public class UserRefreshTokenMapping : IEntityTypeConfiguration<UserRefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<UserRefreshToken> builder)
+        {
+            builder.HasKey(urt => urt.UserId);
+            builder.Property(urt => urt.Code).IsRequired().HasMaxLength(200);
+        }
+    }
+
+}
