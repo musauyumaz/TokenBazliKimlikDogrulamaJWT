bb814d241786fee0fb2a7197dd37745b134952a2
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-11-19 21:05:01 +0300

    feat: add product repository

---
+++ AuthServer/src/server/Infrastructure/AuthServer.Persistence/Repositories/Products/ProductWriteRepository.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Application.Abstractions.Repositories;
+using AuthServer.Domain.Entities;
+using AuthServer.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AuthServer.Persistence.Repositories.Products;
+
+public sealed class ProductWriteRepository(AuthServerDbContext _authServerDbContext) : IWriteRepository<Product>
+{
+    public DbSet<Product> Table => _authServerDbContext.Set<Product>();
+    public async Task AddAsync(Product entity) => await Table.AddAsync(entity);
+    public async Task AddRangeAsync(IEnumerable<Product> entities) => await Table.AddRangeAsync(entities);
+    public async Task<int> ExecuteDeleteAsync(Expression<Func<Product, bool>> predicate) => await Table.Where(predicate).ExecuteDeleteAsync();
+    public async Task<int> SaveChangesAsync() => await _authServerDbContext.SaveChangesAsync();
+}
