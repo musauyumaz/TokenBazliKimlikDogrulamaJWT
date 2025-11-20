2f953ad623335a2962390c6ba696fcedb8508e39
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-11-20 21:46:31 +0300

    feat: add db context to service registration

--- AuthServer/src/server/Infrastructure/AuthServer.Persistence/ServiceRegistration.cs
+++ AuthServer/src/server/Infrastructure/AuthServer.Persistence/ServiceRegistration.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AuthServer.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace AuthServer.Persistence
+namespace AuthServer.Persistence;
+
+public static class ServiceRegistration
 {
-    public static class ServiceRegistration
+    public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
-        public static void AddPersistenceServices(this IServiceCollection services)
-        {
-            
-        }   
-    }
-
+        services.AddDbContext<AuthServerDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("SqlConnection")));
+    }   
 }
