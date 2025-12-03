4ee194e86931c2d9ef9828a382b6912b03cee0b4
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-03 21:13:44 +0300

    feat: update ITokenService to use Client DTO

--- AuthServer/src/server/Core/AuthServer.Application/Abstractions/Services/ITokenService.cs
+++ AuthServer/src/server/Core/AuthServer.Application/Abstractions/Services/ITokenService.cs
@@ -1,11 +1,12 @@
 ﻿using AuthServer.Application.Features.Auths.DTOs;
 using AuthServer.Domain.Entities;
+using AuthServer.Infrastructure.Services.Tokens.Configurations;
 
 namespace AuthServer.Application.Abstractions.Services
 {
     public interface ITokenService
     {
         TokenDTO CreateTokenAsync(User user);
-        ClientTokenDTO CreateTokenByClientAsync(string userId, string password);
+        ClientTokenDTO CreateTokenByClientAsync(Client client);
     }
 }
