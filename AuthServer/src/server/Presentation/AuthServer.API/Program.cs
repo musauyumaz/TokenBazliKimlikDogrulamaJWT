0bb4ef7d000a686c4626e76c6ca7add299ce6a90
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-05 18:26:11 +0300

    feat: add JWT authentication

--- AuthServer/src/server/Presentation/AuthServer.API/Program.cs
+++ AuthServer/src/server/Presentation/AuthServer.API/Program.cs
@@ -1,6 +1,10 @@
 using AuthServer.Application;
 using AuthServer.Infrastructure;
+using AuthServer.Infrastructure.DTOs;
 using AuthServer.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +12,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+{
+    var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<CustomTokenOption>();
+    options.TokenValidationParameters = new()
+    {
+        ValidIssuer = tokenOptions.Issuer,
+        ValidAudiences = tokenOptions.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenOptions.SecurityKey)),
+
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
