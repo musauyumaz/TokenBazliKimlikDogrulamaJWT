0ec5bb6febc0538b5b8e20b0445f6f43db966b2d
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-06 17:33:55 +0300

    feat: initialize BaseEntity Id with Guid

--- AuthServer/src/server/Core/AuthServer.Domain/Entities/Commons/BaseEntity.cs
+++ AuthServer/src/server/Core/AuthServer.Domain/Entities/Commons/BaseEntity.cs
@@ -2,7 +2,7 @@
 {
     public abstract class BaseEntity : IIsActive, ICreated, IUpdated
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
         public bool IsActive { get; set; }
