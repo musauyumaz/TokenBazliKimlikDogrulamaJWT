378777fb9d7715d5b3f8163b19436d8c72ad5d9d
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-06 10:38:47 +0300

    feat: add users controller with create and get user endpoints

---
+++ AuthServer/src/server/Presentation/AuthServer.API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using AuthServer.Application.Features.Users.Commands;
+using AuthServer.Application.Features.Users.DTOs;
+using AuthServer.Application.Features.Users.Queries;
+using Mediator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Results;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(IMediator _mediator) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserCommandRequest createUserCommandRequest)
+        {
+            Result<UserDTO> result = await _mediator.Send(createUserCommandRequest);
+            return StatusCode(((int)result.StatusCode), result);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUserByName(GetUserByNameQueryRequest getUserByNameQueryRequest)
+        {
+            Result<UserDTO> result = await _mediator.Send(getUserByNameQueryRequest);
+            return StatusCode(((int)result.StatusCode), result);
+        }
+    }
+}
