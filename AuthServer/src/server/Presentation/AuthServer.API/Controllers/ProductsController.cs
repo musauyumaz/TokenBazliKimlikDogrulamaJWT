47a2873e595081d97e125ae7b7a0bf6e09c2b98a
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-06 14:00:21 +0300

    feat: add ProductsController

---
+++ AuthServer/src/server/Presentation/AuthServer.API/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using AuthServer.Application.Features.Products.Commands;
+using AuthServer.Application.Features.Products.DTOs;
+using AuthServer.Application.Features.Products.Queries;
+using Mediator;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Results;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(IMediator _mediator) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(AddProductCommandRequest command)
+        {
+            Result result = await _mediator.Send(command);
+            return StatusCode((int)result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts(GetAllProductsQueryRequest query)
+        {
+            Result<List<ProductDTO>> result = await _mediator.Send(query);
+            return StatusCode((int)result.StatusCode, result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(UpdateProductCommandRequest command)
+        {
+            Result result = await _mediator.Send(command);
+            return StatusCode((int)result.StatusCode, result);
+        }
+
+        [HttpDelete("/{Id}")]
+        public async Task<IActionResult> DeleteProduct(DeleteProductCommandRequest command)
+        {
+            Result result = await _mediator.Send(command);
+            return StatusCode((int)result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetByIdProduct(GetByIdProductCommandRequest query)
+        {
+            Result<ProductDTO> result = await _mediator.Send(query);
+            return StatusCode((int)result.StatusCode, result);
+        }
+    }
+}
